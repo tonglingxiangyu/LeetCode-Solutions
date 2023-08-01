1a0b1331863243e7e84d4a726c27d732dcf3e1e9
tonglingxiangyu <l3250265700@163.com>
2023-08-01 17:50:37 +0800

    update

--- 452. 用最少数量的箭引爆气球/solution.cpp
+++ 452. 用最少数量的箭引爆气球/solution.cpp
@@ -1,46 +1,17 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-using namespace std;
-#include <vector>
-#include <bits/stdc++.h>
-
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        /*
-            1.子序列可以是不连续的；子数组（子字符串）需要是连续的；
-            2.动态规划也是有套路的：单个数组或者字符串要用动态规划时，可以把动态规划 dp[i] 定义为 nums[0:i] 中想要求的结果；当两个数组或者字符串要用动态规划时，可以把动态规划定义成两维的 dp[i][j] ，其含义是在 A[0:i] 与 B[0:j] 之间匹配得到的想要的结果。
-            3.对于本题而言，可以定义 dp[i][j] 表示 text1[0:i-1] 和 text2[0:j-1] 的最长公共子序列。 （注：text1[0:i-1] 表示的是 text1 的 第 0 个元素到第 i - 1 个元素，两端都包含） 之所以 dp[i][j] 的定义不是 text1[0:i] 和 text2[0:j] ，是为了方便当 i = 0 或者 j = 0 的时候，dp[i][j]表示的为空字符串和另外一个字符串的匹配，这样 dp[i][j] 可以初始化为 0.
-            4.状态转移方程：
-            当 text1[i - 1] == text2[j - 1] 时，说明两个子字符串的最后一位相等，所以最长公共子序列又增加了 1，所以 dp[i][j] = dp[i - 1][j - 1] + 1
-            当 text1[i - 1] != text2[j - 1] 时，说明两个子字符串的最后一位不相等，那么此时的状态 dp[i][j] 应该是 dp[i - 1][j] 和 dp[i][j - 1] 的最大值。
-        */
-        int m = text1.size();
-        int n = text2.size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1));
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int n = points.size();
+        if(n == 0) return 0;
+        sort(points.begin(), points.end(), [](const auto& u, const auto& v){return u[1] < v[1];});
+        int right = points[0][1];
+        int ans = 1;
+        for(int i = 1; i < n; i++){
+            if(right < points[i][0]){
+                right = points[i][1];
+                ans++;
             }
         }
-        return dp[m][n];
+        return ans;
     }
 };
-
- 
-int main(){
-    string text1 = "abcde";
-    string text2 = "ace";
-
-    Solution solution;
-    int result = solution.longestCommonSubsequence(text1, text2);
-    cout << "results: " << result << endl;
-}
-
-
